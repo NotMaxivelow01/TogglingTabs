1925d093584d7bca30a3ea29261665b99477aeed
NotMaxivelow01 <mfont@endenschool.fr>
2022-11-29 14:37:57 +0100

    l

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
+
+
+function Activate(btn) {
+    btn.style.backgroundColor = "blue";
+    btn.style.color = "white";
+    btn.style.border = "solid blue 1px";
+}
+
+
+button1.addEventListener("click", () => {
+
+})
