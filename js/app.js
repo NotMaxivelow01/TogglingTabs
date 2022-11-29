e394ecddc039f91aaf7ab72f5ffa93c7b549c5b9
NotMaxivelow01 <mfont@endenschool.fr>
2022-11-29 17:15:44 +0100

    lllllll

--- js/app.js
+++ js/app.js
@@ -1,15 +1,53 @@
-const button1 = document.getElementById('button1');
-const button2 = document.getElementById('button2');
-const button3 = document.getElementById('button3');
+// const button1 = document.getElementById('button1');
+// const button2 = document.getElementById('button2');
+// const button3 = document.getElementById('button3');
 
+// let lastSelected = '';
 
-function Activate(btn) {
-    btn.style.backgroundColor = "blue";
-    btn.style.color = "white";
-    btn.style.border = "solid blue 1px";
-}
+// function On(btn) {
+//     btn.style.backgroundColor = "#5796ff";
+//     btn.style.color = "white";
+//     btn.style.border = "solid #d9e6fc 2px";
+// }
 
+// function Off(btn) {
+//     btn.style.backgroundColor = "white";
+//     btn.style.color = "black";
+//     btn.style.border = "solid #5796ff 2px";
+// }
 
-button1.addEventListener("click", () => {
+// button1.addEventListener("click", () => {
+//     if (lastSelected !== "") {
+//         Off(document.getElementById(lastSelected));
+//         On(button1);
+//         lastSelected = "button1";
+//     } else {
+//         On(button1);
+//         lastSelected = "button1";
+//     }
+//     console.log(lastSelected);
+// })
 
-})
+// button2.addEventListener("click", () => {
+//     if (lastSelected !== "") {
+//         Off(document.getElementById(lastSelected));
+//         On(button2);
+//         lastSelected = "button2";
+//     } else {
+//         On(button2);
+//         lastSelected = "button2";
+//     }
+//     console.log(lastSelected);
+// })
+// button3.addEventListener("click", () => {
+//     if (lastSelected !== "") {
+//         Off(document.getElementById(lastSelected));
+//         On(button3);
+//         lastSelected = "button3";
+//     } else {
+//         On(button3);
+//         lastSelected = 'button3';
+//     } 
+
+//     console.log(lastSelected);
+// })
