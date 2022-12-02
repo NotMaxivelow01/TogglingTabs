c79fd8868435081ee4b3339e2d276ac39c84ed7f
NotMaxivelow01 <mfont@endenschool.fr>
2022-12-02 15:13:59 +0100

    final push

--- js/app.js
+++ js/app.js
@@ -1,44 +1,37 @@
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
 const button3 = document.getElementById('button3');
-
-const option1 = document.getElementById('option1');
-const option2 = document.getElementById('option2');
-const option3 = document.getElementById('option3');
-
+const champ = document.getElementById('Selects');
+// const option1 = document.getElementById('option1');
+// const option2 = document.getElementById('option2');
+// const option3 = document.getElementById('option3');
+console.log(champ.value);
 let last = '';
 
 let lastSelected = '';
 
+
 function On(btn) {
     btn.style.backgroundColor = "#5796ff";
     btn.style.color = "white";
-    btn.style.border = "solid #d9e6fc 2px";
 }
 
 function Off(btn) {
     btn.style.backgroundColor = "white";
     btn.style.color = "black";
-    btn.style.border = "solid #5796ff 2px";
-    
 }
 
-function Unselected (btn) {
-    btn.style.backgroundColor = "#5796ff";
-    btn.style.color = "white";
-}
 
 button1.addEventListener("click", () => {
     if (lastSelected !== "") {
         Off(document.getElementById(lastSelected));
         On(button1);
         lastSelected = "button1";
-        Unselected(option1)
     } else {
         On(button1);
         lastSelected = "button1";
     }
-    console.log(lastSelected);
+    champ.value = button1.value
 })
 
 button2.addEventListener("click", () => {
@@ -50,7 +43,7 @@ button2.addEventListener("click", () => {
         On(button2);
         lastSelected = "button2";
     }
-    console.log(lastSelected);
+    champ.value = button2.value
 })
 button3.addEventListener("click", () => {
     if (lastSelected !== "") {
@@ -59,13 +52,27 @@ button3.addEventListener("click", () => {
         lastSelected = "button3";
     } else {
         On(button3);
-        lastSelected = 'button3';
+        lastSelected = "button3";
     }
- 
-    console.log(lastSelected);
+    champ.value = button3.value
 })
-button1.style.hover.backgroundColor = "#d9e6fc";
 
-// option1.addEventListener("click", () => {
-    
-// })
+champ.addEventListener('click', () => {
+    if (champ.value === "Sortbyname") {
+        On(button2);
+        Off(button1);
+        Off(button3);
+        lastSelected = "button2"
+    } else if (champ.value === "Sortbyprice") {
+        On(button1);
+        Off(button2);
+        Off(button3);
+        lastSelected = "button1"
+    } else if (champ.value === "Sortbyrelevance") {
+        On(button3);
+        Off(button2);
+        Off(button1);
+        lastSelected = "button3"
+    }
+});
+
